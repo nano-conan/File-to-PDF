83629ff8af90ba80eb39c0f018dfd34fa70bfda8
Conan <csimon@unb.ca>
2022-04-27 08:23:30 -0300

    updating README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # File-to-PDF
 Converts a file to PDF
+
+Currently only has support for plain text files.
--- topdf.go
+++ topdf.go
@@ -5,27 +5,42 @@ import (
 	"os"
 	"io/ioutil"
 	"log"
+	"strings"
 
 	"github.com/jung-kurt/gofpdf"
 
 )
 
 func main() {
-	file := "test.txt"
+	args := os.Args[1:]
+	numOfArgs := len(args)
 
-	content, err := ioutil.ReadFile(file)
+	for i := 0; i < numOfArgs; i++ {
 
-	if err != nil {
-		log.Fatalf("%s file not found", file)
+		file := args[i]
+
+		content, err := ioutil.ReadFile(file)
+
+		if err != nil {
+			log.Println(fmt.Sprintf("%s file not found", file))
+			continue
+		}
+
+		pdf := gofpdf.New("P", "mm", "A4", "")
+		pdf.AddPage()
+		pdf.SetFont("Arial", "B", 14)
+
+		pdf.MultiCell(190, 5, string(content), "0", "0", false)
+
+		fileName := excludeFileExtension(args[i])
+		_ = pdf.OutputFileAndClose(fmt.Sprintf("%s.pdf", fileName))
+
+		fmt.Println("PDF Created")
+
 	}
 
-	pdf := gofpdf.New("P", "mm", "A4", "")
-	pdf.AddPage()
-	pdf.SetFont("Arial", "B", 14)
+}
 
-	pdf.MultiCell(190, 5, string(content), "0", "0", false)
-
-	_ = pdf.OutputFileAndClose("test.pdf")
-
-	fmt.Println("PDF Created")
+func excludeFileExtension(file string) string {
+	return strings.Split(file, ".")[0]
 }
